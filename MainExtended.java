d2febdf53cd83d94b1ab1bcecfdf6413fbdf3060
Kowal14 <111987438+Kowal14@users.noreply.github.com>
2022-08-29 05:58:48 +0200

    Add files via upload

---
+++ MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Cześć! Mam na imię Marcin";
+        int myAge = 36;
+        double myHeight = 2.00;
+        int myWeight = 110;
+        char favouriteLetter = 'X';
+        boolean isGoodPerson = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(myWeight);
+        System.out.println(favouriteLetter);
+        System.out.println(isGoodPerson);
+
+    }
+}
