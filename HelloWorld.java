65226fcd9cebc5f3cb2adaf97f9f052a3d20ccfb
Kowal14 <111987438+Kowal14@users.noreply.github.com>
2022-09-12 14:24:34 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String invitation = "Hello World";
+        String myName = "Marcin";
+        int myAge = 36;
+        char firstLetterOfSurname = 'K';
+        System.out.println(invitation);
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(firstLetterOfSurname);
+
+
+
+
+    }
+}
