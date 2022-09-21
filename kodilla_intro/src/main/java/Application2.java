3f4eb1c08342b4a7ae9d74c796d9a10207eaee82
Marcin Kowalewski <kowal1014@o2.pl>
2022-09-21 09:17:06 +0200

    Initial version

---
+++ kodilla_intro/src/main/java/Application2.java
@@ -0,0 +1,6 @@
+public class Application2 {
+    public static void main(String[] args) {
+        Application application = new Application("Adam", 40.5, 170);
+        application.checkUser();
+    }
+}
