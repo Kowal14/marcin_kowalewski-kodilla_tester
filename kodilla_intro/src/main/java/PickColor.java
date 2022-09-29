3e6301addb321f648c91eb4862091c4977c66978
Marcin Kowalewski <kowal1014@o2.pl>
2022-09-29 18:57:47 +0200

    Initial version

--- kodilla_intro/src/main/java/PickColor.java
+++ kodilla_intro/src/main/java/PickColor.java
@@ -1,7 +1,7 @@
 public class PickColor {
     public static void main(String[] args) {
-UserDialogs color = new UserDialogs();
-String result = UserDialogs.pickAcolor();
-System.out.println(result);
+        UserDialogs color = new UserDialogs();
+        String result = UserDialogs.pickAcolor();
+        System.out.println(result);
     }
 }
--- kodilla_intro/src/main/java/RandomNumbers.java
+++ kodilla_intro/src/main/java/RandomNumbers.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class RandomNumbers {
     public int min = 30;
     public int max;
+
     public int getCountofRandomNumbers() {
         Random random = new Random();
         int result = 0;
@@ -11,10 +12,12 @@ public class RandomNumbers {
             int temp = random.nextInt(31);
             sum = sum + temp;
             result++;
-            if(temp > max) {
+
+            if (temp > max) {
                 max = temp;
             }
-            if(temp < min) {
+
+            if (temp < min) {
                 min = temp;
             }
         }
