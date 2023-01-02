cb82b645aca3cb5cc558c861d0af249ab3524a7d
Marcin Kowalewski <kowal1014@o2.pl>
2023-01-02 13:07:50 +0100

    Zadanie 6.4

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -8,7 +8,9 @@ import java.util.List;
 public class Warehouse {
     public static void main(String[] args) {
         getOrder();
+
     }
+
     public static List<Order> getOrdersList() {
         List<Order> orders = new ArrayList<>();
         orders.add(new Order("321"));
---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -0,0 +1,8 @@
+package com.kodilla.exception.homework;
+
+public class WarehouseApp {
+    public static void main(String[] args) {
+
+
+    }
+}
